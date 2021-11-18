20d9dbf34418269e773047b90e37a26d2120a4fc
petya <mityushin_petya@mail.ru>
2021-11-18 20:08:02 +0300

    create main menu

---
+++ app/src/main/java/com/example/localchess/mainmenu/MainMenuActivity.kt
@@ -0,0 +1,23 @@
+package com.example.localchess.mainmenu
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.localchess.R
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun onClickCreateHost(view: View) {
+
+    }
+
+    fun onClickConnect(view: View)
+    {
+
+    }
+}
